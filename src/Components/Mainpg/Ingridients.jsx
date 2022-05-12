c5a2c5ce476ddd6e244d72aa3116ccedfc9ce0e1
lostofan <kolog007@gmail.com>
2022-05-12 16:14:36 +0300

    logic-first

--- src/Components/Mainpg/Ingridients.jsx
+++ src/Components/Mainpg/Ingridients.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 export default function Ingridients(props) {
-  const cssshit = function(){
-    this.style.outline = '1px solid #4e60ff';
-  }
+
+  
   return (
-    <div className="main__ingridients_block" onClick={cssshit}>
+    <div className="main__ingridients_block" onClick={props.myfn}>
         <img src={require(`../../imgs/${props.link}.png`)} alt="" />
         <p>{props.name}</p>
     </div>
--- src/Components/Mainpg/Main.jsx
+++ src/Components/Mainpg/Main.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Discount from './Discount'
 import Ingridients from './Ingridients'
 import Restaurant from './Restaurant'
 
 export default function Main() {
+
+  const [blockClick, setBlockClick] = useState(false);
+
+  const useStyle = (event) => {
+    if (blockClick === false) { 
+      setBlockClick(true);
+    event.currentTarget.classList.add("main__ingridients_block_clicked");
+    getIngridientName(event);
+    } 
+    else {  
+      setBlockClick(false);
+      event.currentTarget.classList.remove("main__ingridients_block_clicked");
+    }
+  }
+
+  const getIngridientName = (event) => {
+    /* compareAgain (
+      event.currentTarget.lastChild.innerHTML.toLowerCase()
+      ); */
+  }
+/* const compareAgain = (result) => {
+for(let x = 0; )
+}
+
+  const compareIngridients = (result) => {
+    kitchen.map (
+      (obj, key) => (if (result === obj) {
+
+      })
+    )
+  }
+ */
+  const kitchenArray = [
+["sushi"],
+["burger", "pizza"],
+["sushi"],
+["sushi"],
+["sushi", "vegan"],
+["sushi"],
+  ];
+
+
+
+
   return (
     <main>
       <div className="main__wrapper">
@@ -12,22 +56,22 @@ export default function Main() {
           <Discount num="2" name="WWW" bgcol="#FFF3ED" article="Big Burgers" sum="50% OFF" type="Foodies"/>
         </div>
         <div className="main__ingridients">
-          <Ingridients name="Pizza" link="pizza" />
-          <Ingridients name="Burger" link="burger"/>
-          <Ingridients name="BBQ" link="meat"/>
-          <Ingridients name="Sushi" link="sushi"/>
-          <Ingridients name="Vegan" link="broccoli"/>
-          <Ingridients name="Desserts" link="cake"/>
+          <Ingridients myfn={useStyle} name="BBQ" link="bbq"/>
+          <Ingridients myfn={useStyle} name="Burger" link="burger"/>
+          <Ingridients myfn={useStyle} name="Pizza" link="pizza" />
+          <Ingridients myfn={useStyle} name="Sushi" link="sushi"/>
+          <Ingridients myfn={useStyle} name="Vegan" link="vegan"/>
+          <Ingridients myfn={useStyle} name="Desserts" link="desserts"/>
         </div>
         <div className="main__restaurant">
           <div className="main__article"><p>Nearby restaurants</p></div>
           <div className="wrapper__main_restaurant">
-          <Restaurant pic="royal" name="Royal Sushi House" time="30-40 min" sum="$32 min sum" featured="true" />
-          <Restaurant pic="burg" name="Burgers & Pizza" time="40-60 min" sum="$24 min sum" featured="true" />
-          <Restaurant pic="ninja" name="Ninja sushi" time="20-40 min" sum="$40 min sum" />
-          <Restaurant pic="master" name="Sushi master" time="60-70 min" sum="$49 min sum" />
-          <Restaurant pic="jap" name="Japanese sushi" time="30-50 min" sum="$104 min sum" />
-          <Restaurant pic="kobe" name="Kobe" time="20-30 min" sum="$57 min sum" />
+          <Restaurant pic="royal" name="Royal Sushi House" time="30-40 min" sum="$32 min sum" kitchen={kitchenArray[0]} featured="true" />
+          <Restaurant pic="burg" name="Burgers & Pizza" time="40-60 min" sum="$24 min sum" kitchen={kitchenArray[1]} featured="true" />
+          <Restaurant pic="ninja" name="Ninja sushi" time="20-40 min" sum="$40 min sum" kitchen={kitchenArray[2]} />
+          <Restaurant pic="master" name="Sushi master" time="60-70 min" sum="$49 min sum" kitchen={kitchenArray[3]} />
+          <Restaurant pic="jap" name="Japanese sushi" time="30-50 min" sum="$104 min sum" kitchen={kitchenArray[4]} />
+          <Restaurant pic="kobe" name="Kobe" time="20-30 min" sum="$57 min sum" kitchen={kitchenArray[5]} />
           </div>
         </div>
       </div>
