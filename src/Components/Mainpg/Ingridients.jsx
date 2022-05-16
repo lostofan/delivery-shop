03a460af87360a1bcdb4a32f815d27dc8c4a9f1e
lostofan <kolog007@gmail.com>
2022-05-16 17:36:29 +0300

    badcoded and bugged but works lol

--- src/Components/Mainpg/Ingridients.jsx
+++ src/Components/Mainpg/Ingridients.jsx
@@ -1,9 +1,18 @@
 import React, {useState} from 'react'
 
+const cache = {
+  bbq: [],
+  burger: [],
+  pizza: [],
+  sushi: [],
+  vegan: [],
+  desserts: [],
+}
+
 export default function Ingridients(props) {
   const [blockClick, setblockClick] = useState(false);
   return (
-    <div className="main__ingridients_block" onClick={(event) => {props.myfn(event, blockClick, setblockClick)}}>
+    <div className="main__ingridients_block" id={"ingridients_"+props.id} onClick={(event) => {props.myfn(event, blockClick, setblockClick, cache)}}>
         <img src={require(`../../imgs/${props.link}.png`)} alt="" />
         <p>{props.name}</p>
     </div>
